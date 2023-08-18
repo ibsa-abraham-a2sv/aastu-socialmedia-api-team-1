296a489acc85c1f4340c1c1fe9ed6f32d2e8ef58
Abdulsemed-Muhaba <abdulsemed.muhaba@a2sv.org>
2023-08-18 09:30:09 +0300

    add features in folder structure

--- SocialMediaApp.Application/ApplicationServicesRegistration.cs
+++ SocialMediaApp.Application/ApplicationServicesRegistration.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-
+using MediatR;
 namespace SocialMediaApp.Application;
 
 public static class ApplicationServicesRegistration
