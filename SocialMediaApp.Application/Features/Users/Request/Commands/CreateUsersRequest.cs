38fc34c28f5fef682d7172495bd87a8a3feb819d
Abdulsemed-Muhaba <abdulsemed.muhaba@a2sv.org>
2023-08-18 09:52:09 +0300

    add features folder

---
+++ SocialMediaApp.Application/Features/Users/Request/Commands/CreateUsersRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMediaApp.Application.Features.Users.Request.Commands;
+
+public class CreateUsersRequest
+{
+}
