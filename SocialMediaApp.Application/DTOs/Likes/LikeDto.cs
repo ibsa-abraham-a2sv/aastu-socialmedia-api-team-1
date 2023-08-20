fa879383a2c324c6264cb6d61086aa1079576f0f
keba2sv <kibrom.hailu@a2sv.org>
2023-08-20 09:54:08 +0300

    Complete The domain structure of post and like

--- SocialMediaApp.Application/DTOs/Likes/LikeDto.cs
+++ SocialMediaApp.Application/DTOs/Likes/LikeDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SocialMediaApp.Application.DTOs.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,6 @@ using System.Threading.Tasks;
 
 namespace SocialMediaApp.Application.DTOs.Likes;
 
-public class LikeDto
+public class LikeDto : BaseDto
 {
 }
