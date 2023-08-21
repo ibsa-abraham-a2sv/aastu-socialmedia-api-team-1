1c4c6fdf824af278cdbbf1a7205a9dd339381a76
kibrom-hailu <kibrom.hailu@a2sv.org>
2023-08-21 17:28:06 +0300

    finished on Like Repository and Post Repository

--- SocialMediaApp.Application/DTOs/Posts/Validators/IPostDtoValidator.cs
+++ SocialMediaApp.Application/DTOs/Posts/Validators/IPostDtoValidator.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FluentValidation;
+using SocialMediaApp.Application.Persistence.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,21 @@ using System.Threading.Tasks;
 
 namespace SocialMediaApp.Application.DTOs.Posts.Validators
 {
-    internal class IPostDtoValidator
+    public class IPostDtoValidator : AbstractValidator<IPostDto>
     {
+        private readonly IPostRepository _likeRepository;
+        public IPostDtoValidator(IPostRepository likeRepository)
+        {
+            _likeRepository = likeRepository;
+            RuleFor(n => n.UserId)
+            .GreaterThan(0)
+            .MustAsync(async (id, token) =>
+            {
+                var UserIdExists = await _likeRepository.Exists(id);
+                return !UserIdExists;
+            })
+            .WithMessage("{PropertyName} does not exist.");
+
+        }
     }
 }
