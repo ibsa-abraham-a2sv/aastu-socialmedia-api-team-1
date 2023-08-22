1899ced7a6e32afd9ad617183ee286af762160fb
kibrom-hailu <kibrom.hailu@a2sv.org>
2023-08-22 17:40:40 +0300

    add userId to the post base entitiy

--- SocialMediaApp.Domain/Post.cs
+++ SocialMediaApp.Domain/Post.cs
@@ -9,10 +9,13 @@ namespace SocialMediaApp.Domain;
 
 public class Post:BaseEntity
 {
+    public int UserId { get; set; }
+
     public string? Title { get; set; }   
     public string? Content { get; set; }
     public List<Comment>? Comments { get; set; }
     public List<Like>? Like { get; set; }
+
     public List<String>? HashTag { get; set; }
 
 
