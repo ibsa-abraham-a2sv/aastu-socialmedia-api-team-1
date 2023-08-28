3dd3c2d8f910e49b510a207fb689b88684e35e5a
jima-J9M1 <jima@a2sv.org>
2023-08-28 09:27:42 +0300

    Add testing

--- test/UnitTest/Mocks/MockRepositoryFactory.cs
+++ test/UnitTest/Mocks/MockRepositoryFactory.cs
@@ -118,7 +118,7 @@ public static class MockRepositoryFactory
                     return user.Any();
                 });
 
-                mockRepo.Setup(r => r.Update(It.IsAny<User>()));
+                mockRepo.Setup(r => r.Update(Itw.IsAny<User>()));
                 
                 return mockRepo;
 
